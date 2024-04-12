b2819ff18f5eeaa916a9d6051b3461b4151785c8
luisda <lusanchezmo@unal.edu.co>
2024-04-12 09:26:49 -0500

    port 3000

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config({ path: './../.env' })
 const express = require('express')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT 
 
 const routes = require('./api/endPoints')
 const cors = require('cors');
