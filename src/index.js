1060f566bb316438ffe7a21a6eb62e65c897c04f
luisda <lusanchezmo@unal.edu.co>
2024-04-11 20:18:56 -0500

    sadas

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://l0-crud-client.vercel.app"],
     methods: ["GET", "POST"]
 }));
 
