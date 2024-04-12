4cb392a293b90d83212b18942d71f190f361ca39
luisda <lusanchezmo@unal.edu.co>
2024-04-11 21:09:33 -0500

    DB_PORT

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -5,7 +5,8 @@ const dbConnection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER_NAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_NAME
+    database: process.env.DB_DATABASE_NAME,
+    port: process.env.DB_PORT
 })
 
 /*dbConnection.connect((err) => {
